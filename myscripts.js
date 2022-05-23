d6cd07bea4ca38374adad1c50c994f982d424301
Aldo Diaz <diazaldo41@icloud.com>
2022-05-23 00:20:55 -0500

    implemented a function to simulate a round of play between user and a computer

--- myscripts.js
+++ myscripts.js
@@ -4,16 +4,49 @@ function computerPlay(){
 
     switch(computerMove){
         case 0:
-            computerMoveString = 'Rock';
+            computerMoveString = 'rock';
             break;
         case 1: 
-            computerMoveString = 'Paper';
+            computerMoveString = 'paper';
             break;
         default:
-            computerMoveString = 'Scissors';
+            computerMoveString = 'scissors';
     }
-
+    
     return computerMoveString;
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+    
+    if(playerSelection != null){
+
+        playerSelection = playerSelection.toLowerCase();
+
+        if(playerSelection == 'rock' && computerSelection == 'paper'){
+            return("You Lose! Paper beats Rock");
+        }
+        else if(playerSelection == 'rock' && computerSelection == 'scissors'){
+            return("You Win! Rock beats Scissors");
+        }
+        else if(playerSelection == 'paper' && computerSelection == 'rock'){
+            return("You Win! Paper beats Rock");
+        }
+        else if(playerSelection == 'paper' && computerSelection == 'scissors'){
+            return("You Lose! Scissors beats Paper");
+        }
+        else if(playerSelection == 'scissors' && computerSelection == 'rock'){
+            return("You Lose! Rock beats Scissors");
+        }
+        else if(playerSelection == 'scissors' && computerSelection == 'paper'){
+            return("You Win! Scissors beats Paper");
+        }
+        else{
+            return("Tie");
+        }
+
+    }
+}
+
+let playerSelection = prompt("Choose Rock, Paper, or Scissors.");
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
